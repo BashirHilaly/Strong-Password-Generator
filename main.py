ae6fd640c118a34f0a22842f30a83ef3d3d32dda
BashirHilaly <54788744+BashirHilaly@users.noreply.github.com>
2021-02-10 12:10:40 -0500

    Added limits to character length

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import string
 import random
 
-
 # Our lists of possible characters for our password
 letters_and_numbers = list(string.ascii_lowercase + string.ascii_uppercase + string.digits)
 
@@ -9,29 +8,39 @@ all_characters = list(string.ascii_lowercase + string.ascii_uppercase + string.d
 
 
 # generate the password first by checking our bool
-def generate_password(num_of_char):
+def generate_password():
     code = []
 
+
+    # Check how many characters
+    characters = int(input("How many characters: "))
+
+    if characters > 999:
+        print("Too Long")
+        generate_password()
+        return
+
+
     check = input("Will your password contain symbols? y/n: ")
+
     if check == "y":
-        for char in range(num_of_char):
+        for char in range(characters):
             code.append(random.choice(all_characters))
         generate_password.final = ''.join(code)
     elif check == "n":
-        for char in range(num_of_char):
+        for char in range(characters):
             code.append(random.choice(letters_and_numbers))
         generate_password.final = ''.join(code)
     else:
         print("You must hit either the 'y' key for yes or the 'n' key for no, then hit enter")
-        generate_password(num_of_char)
+        generate_password()
 
-    
-# Check how many characters
-characters = int(input("How many characters: "))
 
-generate_password(characters)
+generate_password()
 
 # Set password
 password = generate_password.final
+
+print(password)
 
 print(password)
